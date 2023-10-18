4ed5767bdf5d9591d88cc0ef23eab940834d6610
Ajay <ajaypodal1997@gmail.com>
2023-10-19 01:27:20 +0530

    small changes in dash

--- src/scenes/dashspace/Dashboard.jsx
+++ src/scenes/dashspace/Dashboard.jsx
@@ -5,7 +5,17 @@ import CardContent from "@mui/material/CardContent";
 import Box from "@mui/material/Box";
 import Piechart from "../../components/Piechart";
 import Barchart from "../../components/Barchart";
-import { CardHeader, Divider, Typography } from "@mui/material";
+import {
+  CardHeader,
+  Divider,
+  FormControl,
+  IconButton,
+  InputBase,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import ListAltOutlinedIcon from "@mui/icons-material/ListAltOutlined";
@@ -14,8 +24,20 @@ import ArrowUpwardRoundedIcon from "@mui/icons-material/ArrowUpwardRounded";
 import ArrowDownwardRoundedIcon from "@mui/icons-material/ArrowDownwardRounded";
 import { green } from "@mui/material/colors";
 import BasicTable from "../../components/Datatable";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [duration, setDuration] = useState("");
+  const [days, setDays] = useState("");
+
+  const handleChange = (event) => {
+    setDuration(event.target.value);
+  };
+
+  const handleChange2 = (event) => {
+    setDays(event.target.value);
+  };
   return (
     <Box sx={{ flexGrow: 1, padding: "15px", overflow: "hidden" }}>
       <Grid container rowSpacing={3} columnSpacing={8}>
@@ -127,17 +149,40 @@ export default function Dashboard() {
         {/* Graphs and Chart */}
 
         <Grid item xs={6} md={8}>
-          <Card style={{height:'45vh'}}>
+          <Card style={{ height: "45vh" }}>
             <CardContent>
-              <CardHeader title="Overview" subheader="Monthly Earning" />
+              <Box display="flex" style={{ justifyContent: "space-between" }}>
+                <CardHeader title="Overview" subheader="Monthly Earning" />
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Duration
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={duration}
+                      label="duration"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={"Monthly"}>Monthly</MenuItem>
+                      <MenuItem value={"Quarterly"}>Quarterly</MenuItem>
+                      <MenuItem value={"Half-Yearly"}>Half-Yearly</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Box>
               <Barchart />
             </CardContent>
           </Card>
         </Grid>
         <Grid item xs={6} md={4}>
-          <Card style={{height:'45vh'}}>
+          <Card style={{ height: "45vh" }}>
             <CardContent>
-              <CardHeader title="Customers" subheader="Customers that buy products" />
+              <CardHeader
+                title="Customers"
+                subheader="Customers that buy products"
+              />
               <Piechart />
             </CardContent>
           </Card>
@@ -148,9 +193,42 @@ export default function Dashboard() {
         <Grid item xs={6} md={12}>
           <Card>
             <CardContent>
-            <CardHeader title="Product Sell"/>
-            <Divider/>
-            <BasicTable/>
+              <Box display="flex" style={{ justifyContent: "space-between" }}>
+                <CardHeader title="Product Sell" />
+                <div style={{display:'flex'}}>
+                  <Box
+                    display="flex"
+                    backgroundColor="#ececec !important"
+                    borderRadius="3px"
+                    mr={2}
+                  >
+                    <IconButton type="button" sx={{ p: 1 }}>
+                      <SearchIcon />
+                    </IconButton>
+                    <InputBase sx={{ ml: 0, flex: 1 }} placeholder="Search" />
+                  </Box>
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select2-label">
+                        Days
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select2-label"
+                        id="demo-simple-select2"
+                        value={days}
+                        label="days"
+                        onChange={handleChange2}
+                      >
+                        <MenuItem value={"last 30 days"}>last 30 days</MenuItem>
+                        <MenuItem value={"last 60 days"}>last 60 days</MenuItem>
+                        <MenuItem value={"last 90 days"}>last 90 days</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </div>
+              </Box>
+              <Divider />
+              <BasicTable />
             </CardContent>
           </Card>
         </Grid>
